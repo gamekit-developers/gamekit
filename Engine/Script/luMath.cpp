143a6009e090237a204417e05a6f3f13a33a16fd
snailrose <snailrose@c957766c-2088-11de-b650-e715bd6d7cf1>
2010-03-13 15:47:50 +0000

    Made all math functions local to the class instance

--- Engine/Script/luMath.cpp
+++ Engine/Script/luMath.cpp
@@ -191,17 +191,79 @@ static int luVector3_unm(luObject &L)
 }
 
 
+// ----------------------------------------------------------------------------
+static int luVector3_Normalize(luObject &L)
+{
+    if (!LU_IsVec3(L, 1))
+        return L.pushError("expected Vec3Normalize(Vector3)");
+
+    return L.push(LU_GetVec3Arg(L, 1).normalise());
+}
+
+static int luVector3_Dot(luObject &L)
+{
+    if (!LU_IsVec3(L, 1) || !LU_IsVec3(L, 2))
+        return L.pushError("expected Vec3Dot(Vector3, Vector3)");
+
+    return L.push(LU_GetVec3Arg(L, 1).dotProduct(LU_GetVec3Arg(L, 2)));
+}
+
+static int luVector3_Cross(luObject &L)
+{
+    if (!LU_IsVec3(L, 1) || !LU_IsVec3(L, 2))
+        return L.pushError("expected Vec3Cross(Vector3, Vector3)");
+
+    return LU_Vec3New(L, LU_GetVec3Arg(L, 1).crossProduct(LU_GetVec3Arg(L, 2)));
+}
+
+static int luVector3_Len(luObject &L)
+{
+    if (!LU_IsVec3(L, 1))
+        return L.pushError("expected Vec3Len(Vector3)");
+    return L.push(LU_GetVec3Arg(L, 1).length());
+}
+
+
+static int luVector3_LenSq(luObject &L)
+{
+    if (!LU_IsVec3(L, 1))
+        return L.pushError("expected Vec3LenSq(Vector3)");
+    return L.push(LU_GetVec3Arg(L, 1).squaredLength());
+}
+
+
+static int luVector3_Dist(luObject &L)
+{
+    if (!LU_IsVec3(L, 1) || !LU_IsVec3(L, 2))
+        return L.pushError("expected Vec3Dist(Vector3, Vector3)");
+    return L.push(LU_GetVec3Arg(L, 1).distance(LU_GetVec3Arg(L, 2)));
+}
+
+static int luVector3_DistSq(luObject &L)
+{
+    if (!LU_IsVec3(L, 1) || !LU_IsVec3(L, 2))
+        return L.pushError("expected Vec3DistSq(Vector3, Vector3)");
+    return L.push(LU_GetVec3Arg(L, 1).squaredDistance(LU_GetVec3Arg(L, 2)));
+}
+
 luMethodDef luVector3::Methods[] =
 {
-    {"constructor",     luVector3_new,    LU_NOPARAM, "|fff"},
-    {"__tostring",      luVector3_tstr,   LU_PARAM,   "."},
-    {"__getter",        luVector3_get,    LU_PARAM,   ".s"},
-    {"__setter",        luVector3_set,    LU_PARAM,   ".sf"},
-    {"__add",           luVector3_add,    LU_NOPARAM, ""},
-    {"__sub",           luVector3_sub,    LU_NOPARAM, ""},
-    {"__mul",           luVector3_mul,    LU_NOPARAM, ""},
-    {"__mul",           luVector3_div,    LU_NOPARAM, ""},
-    {"__unm",           luVector3_unm,    LU_NOPARAM, ""},
+    {"constructor",     luVector3_new,          LU_NOPARAM, "|fff"},
+    {"normalize",       luVector3_Normalize,    LU_PARAM,   "."},
+    {"dot",             luVector3_Dot,          LU_PARAM,   ".."},
+    {"cross",           luVector3_Cross,        LU_PARAM,   ".."},
+    {"length",          luVector3_Len,          LU_PARAM,   "."},
+    {"length2",         luVector3_LenSq,        LU_PARAM,   "."},
+    {"distance",        luVector3_Dist,         LU_PARAM,   ".."},
+    {"distance2",       luVector3_DistSq,       LU_PARAM,   ".."},
+    {"__tostring",      luVector3_tstr,         LU_PARAM,   "."},
+    {"__getter",        luVector3_get,          LU_PARAM,   ".s"},
+    {"__setter",        luVector3_set,          LU_PARAM,   ".sf"},
+    {"__add",           luVector3_add,          LU_NOPARAM, ""},
+    {"__sub",           luVector3_sub,          LU_NOPARAM, ""},
+    {"__mul",           luVector3_mul,          LU_NOPARAM, ""},
+    {"__mul",           luVector3_div,          LU_NOPARAM, ""},
+    {"__unm",           luVector3_unm,          LU_NOPARAM, ""},
     {0,0,0,0}
 };
 
@@ -319,77 +381,7 @@ static int luQuat_tstr(luObject &L)
 
 
 
-luMethodDef luQuat::Methods[] =
-{
-    {"constructor",     luQuat_new,     LU_NOPARAM, "|fff"},
-    {"__tostring",      luQuat_tstr,    LU_PARAM,   "."},
-    {"__getter",        luQuat_get,     LU_PARAM,   ".s"},
-    {"__setter",        luQuat_set,     LU_PARAM,   ".sf"},
-    {"__mul",           luQuat_mul,     LU_PARAM,   ".|"},
-    {0,0,0,0}
-};
-
-
-luTypeDef luQuat::Type = {"Quaternion", 0, Methods};
-
-
-// ----------------------------------------------------------------------------
-static int luMath_Vec3Normalize(luObject &L)
-{
-    if (!LU_IsVec3(L, 1))
-        return L.pushError("expected Vec3Normalize(Vector3)");
-
-    return L.push(LU_GetVec3Arg(L, 1).normalise());
-}
-
-static int luMath_Vec3Dot(luObject &L)
-{
-    if (!LU_IsVec3(L, 1) || !LU_IsVec3(L, 2))
-        return L.pushError("expected Vec3Dot(Vector3, Vector3)");
-
-    return L.push(LU_GetVec3Arg(L, 1).dotProduct(LU_GetVec3Arg(L, 2)));
-}
-
-static int luMath_Vec3Cross(luObject &L)
-{
-    if (!LU_IsVec3(L, 1) || !LU_IsVec3(L, 2))
-        return L.pushError("expected Vec3Cross(Vector3, Vector3)");
-
-    return LU_Vec3New(L, LU_GetVec3Arg(L, 1).crossProduct(LU_GetVec3Arg(L, 2)));
-}
-
-static int luMath_Vec3Len(luObject &L)
-{
-    if (!LU_IsVec3(L, 1))
-        return L.pushError("expected Vec3Len(Vector3)");
-    return L.push(LU_GetVec3Arg(L, 1).length());
-}
-
-
-static int luMath_Vec3LenSq(luObject &L)
-{
-    if (!LU_IsVec3(L, 1))
-        return L.pushError("expected Vec3LenSq(Vector3)");
-    return L.push(LU_GetVec3Arg(L, 1).squaredLength());
-}
-
-
-static int luMath_Vec3Dist(luObject &L)
-{
-    if (!LU_IsVec3(L, 1) || !LU_IsVec3(L, 2))
-        return L.pushError("expected Vec3Dist(Vector3, Vector3)");
-    return L.push(LU_GetVec3Arg(L, 1).distance(LU_GetVec3Arg(L, 2)));
-}
-
-static int luMath_Vec3DistSq(luObject &L)
-{
-    if (!LU_IsVec3(L, 1) || !LU_IsVec3(L, 2))
-        return L.pushError("expected Vec3DistSq(Vector3, Vector3)");
-    return L.push(LU_GetVec3Arg(L, 1).squaredDistance(LU_GetVec3Arg(L, 2)));
-}
-
-
-static int luMath_QuatDot(luObject &L)
+static int luQuat_Dot(luObject &L)
 {
     if (!LU_IsQuat(L, 1) || !LU_IsQuat(L, 2))
         return L.pushError("expected QuatDot(Quaternion, Quaternion)");
@@ -397,7 +389,7 @@ static int luMath_QuatDot(luObject &L)
 }
 
 
-static int luMath_QuatNormalize(luObject &L)
+static int luQuat_Normalize(luObject &L)
 {
     if (!LU_IsQuat(L, 1))
         return L.pushError("expected QuatNormalize(Quaternion)");
@@ -407,12 +399,27 @@ static int luMath_QuatNormalize(luObject &L)
 
 
 
+luMethodDef luQuat::Methods[] =
+{
+    {"constructor",     luQuat_new,         LU_NOPARAM, "|fff"},
+    {"dot",             luQuat_Dot,         LU_PARAM,   ".."},
+    {"normalize",       luQuat_Normalize,   LU_PARAM,   "."},
+    {"__tostring",      luQuat_tstr,        LU_PARAM,   "."},
+    {"__getter",        luQuat_get,         LU_PARAM,   ".s"},
+    {"__setter",        luQuat_set,         LU_PARAM,   ".sf"},
+    {"__mul",           luQuat_mul,         LU_PARAM,   ".|"},
+    {0,0,0,0}
+};
+
+
+luTypeDef luQuat::Type = {"Quaternion", 0, Methods};
+
+// ----------------------------------------------------------------------------
+
 static int luMath_LerpF(luObject &L)
 {
     return L.push(gkMathUtils::interp(L.tofloat(1), L.tofloat(2), gkClampf(L.tofloat(3), 0.f, 1.f)));
 }
-
-
 
 static int luMath_Vec3Lerp(luObject &L)
 {
@@ -449,20 +456,6 @@ static int luMath_QuatSlerp(luObject &L)
 
 luMethodDef Math_Methods[] =
 {
-    // Vector3
-
-    {"Vec3Normalize",   luMath_Vec3Normalize,       LU_PARAM, "."},
-    {"Vec3Dot",         luMath_Vec3Dot,             LU_PARAM, ".."},
-    {"Vec3Cross",       luMath_Vec3Cross,           LU_PARAM, ".."},
-    {"Vec3Len",         luMath_Vec3Len,             LU_PARAM, "."},
-    {"Vec3LenSq",       luMath_Vec3LenSq,           LU_PARAM, "."},
-    {"Vec3Dist",        luMath_Vec3Dist,            LU_PARAM, ".."},
-    {"Vec3DistSq",      luMath_Vec3DistSq,          LU_PARAM, ".."},
-
-    // Quaternion
-    {"QuatDot",         luMath_QuatDot,             LU_PARAM, ".."},
-    {"QuatNormalize",   luMath_QuatNormalize,       LU_PARAM, "."},
-
     // interpolation
     {"LerpF",           luMath_LerpF,               LU_PARAM, "fff"},
     {"Vec3Lerp",        luMath_Vec3Lerp,            LU_PARAM, "..f"},
