155748106a9678a802c3ef252fcf19170b3eec3a
harkon.kr <harkon.kr@c957766c-2088-11de-b650-e715bd6d7cf1>
2011-07-15 02:27:37 +0000

    Ogre 1.8 version up to 2899.

--- Ogre-1.8/OgreMain/src/OgreSceneManager.cpp
+++ Ogre-1.8/OgreMain/src/OgreSceneManager.cpp
@@ -3332,7 +3332,6 @@ void SceneManager::renderSingleObject(Renderable* rend, const Pass* pass,
 						if (pass->getStartLight() > 0 &&
 							pass->getStartLight() >= rendLightList.size())
 						{
-							lightsLeft = 0;
 							break;
 						}
 						else
@@ -6536,6 +6535,10 @@ InstanceManager* SceneManager::getInstanceManager( const String &managerName ) c
 //---------------------------------------------------------------------
 void SceneManager::destroyInstanceManager( const String &name )
 {
+	//The manager we're trying to destroy might have been scheduled for updating
+	//while we haven't yet rendered a frame. Update now to avoid a dangling ptr
+	updateDirtyInstanceManagers();
+
 	InstanceManagerMap::iterator i = mInstanceManagerMap.find(name);
 	if (i != mInstanceManagerMap.end())
 	{
@@ -6561,6 +6564,7 @@ void SceneManager::destroyAllInstanceManagers(void)
 	}
 
 	mInstanceManagerMap.clear();
+	mDirtyInstanceManagers.clear();
 }
 //---------------------------------------------------------------------
 size_t SceneManager::getNumInstancesPerBatch( const String &meshName, const String &groupName,
