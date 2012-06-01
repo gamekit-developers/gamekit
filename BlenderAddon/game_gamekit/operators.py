f204b6cc9e8974e5f01e31c70957d336ef5808ce
thomas.trocha@gmail.com <thomas.trocha@gmail.com@c957766c-2088-11de-b650-e715bd6d7cf1>
2012-06-01 01:59:28 +0000

    bugfix saving GamekitStartup.cfg (by BTolputt)

--- BlenderAddon/game_gamekit/operators.py
+++ BlenderAddon/game_gamekit/operators.py
@@ -117,7 +117,7 @@ class GamekitExportStartupFileOperator(bpy.types.Operator):
     
     def invoke(self, context, event):
         self.properties.filepath = "//GamekitStartup.cfg"
-        context.window_manager.add_fileselect(self)
+        context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
     
 
